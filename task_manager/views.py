4a5e1430a29f42a0a1d3557e2468aed9f0dd25a3
LuybovB <grandprinceess7777@gmail.com>
2024-06-03 23:53:03 +0300

    add_db

---
+++ task_manager/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+from django.utils.translation import gettext_lazy as _
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+    extra_context = {
+        'header': _('Task manager'),
+    }
