6883511bbe498df19d88828e03a6b0b7b9c7ad39
LuybovB <grandprinceess7777@gmail.com>
2024-06-07 20:07:22 +0300

    add_form

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class CustomUser(models.Model):
+class CustomUser(AbstractUser):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=100)
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
--- task_manager/views.py
+++ task_manager/views.py
@@ -11,15 +11,12 @@ class IndexView(TemplateView):
         'header': _('Task manager'),
     }
 
-
-
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            # Здесь можно добавить логику для автоматического входа пользователя или перенаправления
-            return redirect('index')  # Предполагается, что 'index' - это имя вашего URL для главной страницы
+            return redirect('root')  # Предполагается, что 'index' - это имя вашего URL для главной страницы
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
