8a251f8c15ae521ad0135c77db82b898c3234f04
LuybovB <grandprinceess7777@gmail.com>
2024-06-30 21:56:34 +0300

    add_form

--- task_manager/tests/test_labels.py
+++ task_manager/tests/test_labels.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from task_manager.models import CustomUser, Status
 from task_manager.read_json import load_data
 import django
+
 django.setup()
 
 
@@ -12,29 +13,21 @@ class LabelCRUDTests(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.user = CustomUser.objects.create_user(
-            username='testuser', password='testpass')
+        self.user = CustomUser.objects.create_user(username='testuser', password='testpass')
         self.status = Status.objects.create(name='Тестовый статус')
 
     def test_create_status(self):
         self.client.force_login(self.user)
-        response = self.client.post(
-            reverse('create_status'),
-            {'name': 'Новый статус'}
-        )
+        response = self.client.post(reverse('create_status'), {'name': 'Новый статус'})
+
         self.assertEqual(response.status_code, 302)
-        self.assertTrue(Status.objects.filter(
-            name='Новый статус').exists()
-                        )
+        self.assertTrue(Status.objects.filter(name='Новый статус').exists())
 
     def test_update_status(self):
         self.client.force_login(self.user)
         status_id = self.status.id
-        response = self.client.post(
-            reverse('update_status',
-                    args=[status_id]),
-            {'name': 'Обновленный статус'}
-        )
+        response = self.client.post(reverse('update_status', args=[status_id]), {'name': 'Обновленный статус'})
+
         self.assertEqual(response.status_code, 302)
         self.status.refresh_from_db()
         self.assertEqual(self.status.name, 'Обновленный статус')
@@ -42,9 +35,7 @@ class LabelCRUDTests(TestCase):
     def test_delete_status(self):
         self.client.force_login(self.user)
         status_id = self.status.id
-        response = self.client.post(
-            reverse('delete_status',
-                    args=[status_id])
-        )
+        response = self.client.post(reverse('delete_status', args=[status_id]))
+
         self.assertEqual(response.status_code, 302)
         self.assertFalse(Status.objects.filter(id=status_id).exists())
