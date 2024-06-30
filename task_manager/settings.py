d7436b3a0d5f84b62db483647dcec80b00b243e1
LuybovB <grandprinceess7777@gmail.com>
2024-06-30 13:39:27 +0300

    add_form

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'task_manager',
-    'widget_tweaks',
+    # 'widget_tweaks',
     'django_bootstrap5',
 ]
 
