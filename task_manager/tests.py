3a1af65688b34e0346b2ee10cbc14a92783a06f1
LuybovB <grandprinceess7777@gmail.com>
2024-07-02 02:37:35 +0300

    add_form

---
+++ task_manager/tests.py
@@ -0,0 +1,65 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+from .models import CustomUser, Status
+from django.contrib.messages import get_messages
+
+
+class UserCRUDTests(TestCase):
+
+    fixtures = ['users_fixture.json']
+
+    def setUp(self):
+        self.client = Client()
+        self.user = CustomUser.objects.get(username='testuser')
+        self.status = Status.objects.create(name='Тестовый статус')
+
+    def test_register(self):
+        url = reverse('login')
+        data = {
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password1': 'newpassword',
+            'password2': 'newpassword'
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(CustomUser.objects.count(), 2)
+
+    def test_user_update(self):
+        self.client.force_login(self.user)
+        url = reverse('user_update', args=[self.user.pk])
+        data = {
+            'username': 'updated_user',
+            'email': 'updateduser@example.com'
+        }
+        response = self.client.post(url, data, follow=True)
+        messages = list(response.wsgi_request._messages)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_delete(self):
+        self.client.login(username='testuser', password='testpassword')
+        url = reverse('user_delete', args=[self.user.pk])
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(CustomUser.objects.count(), 2)
+
+
+    def test_create_status(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('create_status'), {'name': 'Новый статус'})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Status.objects.filter(name='Новый статус').exists())
+
+    def test_update_status(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('update_status', args=[self.status.id]), {'name': 'Обновленный статус'})
+        self.assertEqual(response.status_code, 302)
+        self.status.refresh_from_db()
+        self.assertEqual(self.status.name, 'Обновленный статус')
+
+    def test_delete_status(self):
+        self.client.force_login(self.user)
+        response = self.client.post(reverse('delete_status', args=[self.status.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Status.objects.filter(name='Тестовый статус').exists())
--- task_manager/tests/test_load_users.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from task_manager.models import CustomUser
-
-@pytest.mark.django_db
-def test_load_users():
-    # Предполагаем, что фикстуры загружены здесь
-    assert CustomUser.objects.count() == 3, "Количество пользователей должно быть 3"
