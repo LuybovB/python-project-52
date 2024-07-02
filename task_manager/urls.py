4655c95dfd9db5db9b3964602b1fa458d65d8866
LuybovB <grandprinceess7777@gmail.com>
2024-07-02 17:47:19 +0300

    add_form

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
     path('tasks/<int:pk>/', views.task_detail, name='task_detail'),
     path('labels/', views.labels_list, name='labels-list'),
     path('labels/create/', views.label_create, name='label_create'),
-    path('labels/<int:pk>/edit/', views.label_update, name='label_update'),
+    path('labels/<int:pk>/update/', views.label_update, name='label_update'),
     path('labels/<int:pk>/delete/', views.label_delete, name='label_delete'),
 ]
