d64623aaab2adb9658ea33bad323fba5e42b2237
LuybovB <grandprinceess7777@gmail.com>
2024-07-02 15:32:52 +0300

    add_form

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -101,6 +101,9 @@ class StatusForm(forms.ModelForm):
     class Meta:
         model = Status
         fields = ['name']
+        labels = {
+            'name': 'Имя',
+        }
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control',
                                                     'placeholder': 'Имя'})
